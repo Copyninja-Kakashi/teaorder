593f782652ca984f9aeb51183364094869928fbe
Copyninja-Kakashi <18573635525@163.com>
2019-10-22 15:53:14 +0800

    Initial commit

---
+++ src/main/java/com/newer/domain/TeaOrder.java
@@ -0,0 +1,51 @@
+package com.newer.domain;
+
+import java.sql.Timestamp;
+
+public class TeaOrder {
+    private Integer id;
+    private String detail;
+    private Integer num;
+    private Integer price;
+    private Timestamp orderTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+    }
+}
